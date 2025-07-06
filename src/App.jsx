8b8f1dfbf8456c19f276911e63f09da9327dfb0c
smsarov <smsarov@gmail.com>
2025-07-06 16:14:39 +0300

    stable version #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
 import { Header } from './Header'
-import { Footer } from './Footer'
 import { MainComponent } from './MainComponent'
+
 function App() {
 
   return (
     <>
       <Header></Header>
       <MainComponent></MainComponent>
-      <Footer></Footer>
     </>
   )
 }
